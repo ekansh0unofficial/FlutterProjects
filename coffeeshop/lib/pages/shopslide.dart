6d1dbf7048bc62b19a39946bfc7555531cee86d8
unknown <ekanshmittal04@gmail.com>
2024-06-17 13:55:46 +0530

    Coffee Shop - Commit 3

--- coffeeshop/lib/pages/shopslide.dart
+++ coffeeshop/lib/pages/shopslide.dart
@@ -4,6 +4,7 @@ import 'package:coffeeshop/pages/models/coffee_model.dart';
 import 'package:coffeeshop/pages/models/coffee_shop.dart';
 import 'package:coffeeshop/widgets/shop_item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 class Shop extends StatefulWidget {
@@ -29,23 +30,20 @@ class _ShopState extends State<Shop> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<CoffeeModel>(
-      builder: (context, value, child) => 
-      SafeArea(
-        child: Column(
+    return LayoutBuilder(
+      builder:(context , constraints){
+      return Consumer<CoffeeModel>(
+        builder: (context, value, child) => 
+        Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
               SizedBox(height: 24,),
-              Expanded(
-                flex : 1,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16),
-                  child: Text("How do u like your coffee?" , style: TextStyle(color: Colors.black87, fontSize:22 ),),
-                ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16),
+                child: Text("How do u like your coffee?" , style: TextStyle(color: Colors.black87, fontSize:22 ),),
               ),
-
+        
               Expanded(
-                flex : 5,
                 child: ListView.builder(
                   itemCount: value.coffeeshop.length,
                   itemBuilder: (context , index){
@@ -63,7 +61,8 @@ class _ShopState extends State<Shop> {
               )
           ],
         ),
-      ),
+      );
+      }
     );
   }
 }
--- coffeeshop/lib/widgets/shop_item_widget.dart
+++ coffeeshop/lib/widgets/shop_item_widget.dart
@@ -1,17 +1,41 @@
 //import 'package:coffeeshop/const.dart';
 // ignore_for_file: must_be_immutable, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+
 import 'package:coffeeshop/pages/models/coffee_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:lottie/lottie.dart';
 
-class ShopItem extends StatelessWidget {
+
+class ShopItem extends StatefulWidget {
   
   Coffee item ; 
   void Function()? onAdd;
   void Function()? onRemove;
 
   ShopItem({super.key , required this.item , required this.onAdd , required this.onRemove});
+
+  @override
+  State<ShopItem> createState() => _ShopItemState();
+}
+
+class _ShopItemState extends State<ShopItem> with SingleTickerProviderStateMixin{
+
+  late final AnimationController _controller;
+  bool _isLiked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this , duration: Duration(seconds: 2));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
 
   @override  
   Widget build(BuildContext context) {
@@ -22,15 +46,38 @@ class ShopItem extends StatelessWidget {
         decoration: BoxDecoration(color: Colors.white60, borderRadius: BorderRadius.circular(12)),
         margin: EdgeInsets.all(2),
         child: ListTile(
-          leading: Image.asset(item.image_string, height: 70 ,),
-          title: Text(item.name),
-          subtitle: Text(item.price),
+          leading: Image.asset(widget.item.image_string, height: 70 ,),
+          title: Text(widget.item.name),
+          subtitle: Text(widget.item.price),
           trailing: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-                IconButton(onPressed: onRemove, icon: Icon(Icons.remove)),
-                Text("${item.quantity}"),
-                IconButton(onPressed: onAdd, icon: Icon(Icons.add))
+               
+                GestureDetector(
+                  
+                  onTap:(){
+                        if(_isLiked){
+                          _controller.reverse();
+                          setState(() {
+                            _isLiked = false;
+                          });
+                        }
+                        else{
+                          _controller.forward();
+                          setState(() {
+                            _isLiked = true;
+                          });                          
+                        }
+                  },
+                  child: Container(
+                    
+                    child: LottieBuilder.network("https://lottie.host/82f40bba-3c7d-4050-9624-e2f9bd04f6b4/Vs4fsc9Kcj.json", height: 200,controller: _controller, ))
+
+                ),
+              
+                IconButton(onPressed: widget.onRemove, icon: Icon(Icons.remove)),
+                Text("${widget.item.quantity}"),
+                IconButton(onPressed: widget.onAdd, icon: Icon(Icons.add))
             ],
           ),
         ),
