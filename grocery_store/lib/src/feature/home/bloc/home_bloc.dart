641b45e5b87ecc0543b46d3c24f15368563cdcfe
Ekansh <ekanshmittal04@gmail.com>
2024-07-26 18:22:39 +0530

    Adding Json Functionality and Managing States

--- grocery_store/lib/src/feature/home/bloc/home_bloc.dart
+++ grocery_store/lib/src/feature/home/bloc/home_bloc.dart
@@ -1,24 +1,38 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
+import 'package:flutter/services.dart';
 import 'package:meta/meta.dart';
+
+import '../../../data/models /product.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
+    on<FetchHome>((event, emit) async {
+      emit(HomeLoading());
+      print('current state , $state ,${DateTime.timestamp()}');
+      await Future.delayed(Duration(seconds: 3));
+      final jsonString =
+          await rootBundle.loadString('lib/assets/products.json');
+      final Map<String, dynamic> jsonData = json.decode(jsonString);
+      List<Product> productlist = jsonData['products']
+          .map<Product>((item) => Product.fromJson(item))
+          .toList();
+      emit(HomeLoadSuccess(products: productlist));
+      print('current state , $state , ,${DateTime.timestamp()}');
+    });
     on<HomeEvent>((event, emit) {});
 
-    on<OnClickCart>((event, emit) {
-      print('on cart clicked');
-    });
-    on<OnClickFav>((event, emit) {
-      print('on fav clicked');
-    });
+    on<OnClickCart>((event, emit) {});
+    on<OnClickFav>((event, emit) {});
     on<OnNavigateCart>((event, emit) {
-      print('navigating to cart');
+      emit(HomeToCartState());
     });
     on<OnNavigateFav>((event, emit) {
-      print('navigating to fav');
+      emit(HomeToFavState());
     });
   }
 }
---
+++ grocery_store/linux/build/cmake_install.cmake
@@ -0,0 +1,148 @@
+# Install script for directory: /home/ekanshmittal/Desktop/Ekansh/FlutterProjects/grocery_store/linux
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
+  
+  file(REMOVE_RECURSE "/home/ekanshmittal/Desktop/Ekansh/FlutterProjects/grocery_store/linux/build/bundle/")
+  
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}/usr/local/grocery_store" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/usr/local/grocery_store")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}/usr/local/grocery_store"
+         RPATH "$ORIGIN/lib")
+  endif()
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/usr/local/grocery_store")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/usr/local" TYPE EXECUTABLE FILES "/home/ekanshmittal/Desktop/Ekansh/FlutterProjects/grocery_store/linux/build/intermediates_do_not_run/grocery_store")
+  if(EXISTS "$ENV{DESTDIR}/usr/local/grocery_store" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/usr/local/grocery_store")
+    file(RPATH_CHANGE
+         FILE "$ENV{DESTDIR}/usr/local/grocery_store"
+         OLD_RPATH "/home/ekanshmittal/Desktop/Ekansh/FlutterProjects/grocery_store/linux/flutter/ephemeral:"
+         NEW_RPATH "$ORIGIN/lib")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}/usr/local/grocery_store")
+    endif()
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/usr/local/data/icudtl.dat")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/usr/local/data" TYPE FILE FILES "/home/ekanshmittal/Desktop/Ekansh/FlutterProjects/grocery_store/linux/flutter/ephemeral/icudtl.dat")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/usr/local/lib/libflutter_linux_gtk.so")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/usr/local/lib" TYPE FILE FILES "/home/ekanshmittal/Desktop/Ekansh/FlutterProjects/grocery_store/linux/flutter/ephemeral/libflutter_linux_gtk.so")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/usr/local/lib/")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/usr/local/lib" TYPE DIRECTORY FILES "/home/ekanshmittal/Desktop/Ekansh/FlutterProjects/grocery_store/build/native_assets/linux/")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
+  
+  file(REMOVE_RECURSE "/usr/local/data/flutter_assets")
+  
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/usr/local/data/flutter_assets")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/usr/local/data" TYPE DIRECTORY FILES "/home/ekanshmittal/Desktop/Ekansh/FlutterProjects/grocery_store/build//flutter_assets")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/home/ekanshmittal/Desktop/Ekansh/FlutterProjects/grocery_store/linux/build/flutter/cmake_install.cmake")
+
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/home/ekanshmittal/Desktop/Ekansh/FlutterProjects/grocery_store/linux/build/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
