dbd1942d5bac40ab8cbde5efd749adec70647b14
Ekansh <ekanshmittal04@gmail.com>
2024-12-09 23:56:49 +0530

    Application Setup

---
+++ s_potify/lib/core/config/theme/apptheme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:s_potify/commons/app_colors.dart';
+
+class Apptheme {
+  final lightTheme = ThemeData(
+      brightness: Brightness.light,
+      primaryColor: AppColors.primaryColor,
+      scaffoldBackgroundColor: AppColors.lightBackground,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primaryColor,
+              textStyle:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ))));
+}
